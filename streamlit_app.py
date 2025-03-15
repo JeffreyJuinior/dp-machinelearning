284efbe9050082e520c7b0c2b228e0a924da9bb7
Jeffrey Junior <51208117+JeffreyJuinior@users.noreply.github.com>
2025-03-15 17:36:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title('Machine Learning App')
 
 st.info('This app will predict your obesity level!')
 
-df = pd.read_csv('https://raw.githubusercontent.com/JeffreyJuinior/dp-machinelearning/refs/heads/master/heart.csv')
-df
+st.expander('Data'):
+  st.write('This is a raw data')
+  df = pd.read_csv('https://raw.githubusercontent.com/JeffreyJuinior/dp-machinelearning/refs/heads/master/heart.csv')
+  df
