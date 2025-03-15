771fda732e32e2093ac5b8c811a616ab3941de7d
Jeffrey Junior <51208117+JeffreyJuinior@users.noreply.github.com>
2025-03-15 20:13:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,11 +10,11 @@ with st.expander('**Data**'):
   df = pd.read_csv('https://raw.githubusercontent.com/JeffreyJuinior/dp-machinelearning/refs/heads/master/ObesityDataSet_raw_and_data_sinthetic.csv')
   df
   st.write('**X**')
-  X = df.drop('HeartDisease',axis=1)
+  X = df.drop('NObeyesdad',axis=1)
   X
   st.write('**y**')
-  y = df['HeartDisease']
+  y = df['NObeyesdad']
   y
 
-# with st.expander('**Data Visualization**'):
-#   st.scatter_chart(data=df, x = 'Height', y = 'Weight', color='NObeyesdad')
+with st.expander('**Data Visualization**'):
+  st.scatter_chart(data=df, x = 'Height', y = 'Weight', color='NObeyesdad')
