147a0f51fbbc23d2b0fc3660b350d968a1b54cc5
Jeffrey Junior <51208117+JeffreyJuinior@users.noreply.github.com>
2025-03-15 20:10:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.info('This app will predict your obesity level!')
 
 with st.expander('**Data**'):
   st.write('This is a raw data')
-  df = pd.read_csv('https://raw.githubusercontent.com/JeffreyJuinior/dp-machinelearning/refs/heads/master/heart.csv')
+  df = pd.read_csv('https://raw.githubusercontent.com/JeffreyJuinior/dp-machinelearning/refs/heads/master/ObesityDataSet_raw_and_data_sinthetic.csv')
   df
   st.write('**X**')
   X = df.drop('HeartDisease',axis=1)
