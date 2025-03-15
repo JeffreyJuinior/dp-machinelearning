3d0b0627a5aea9ec1e8e3ab414f287e340aca3d1
Jeffrey Junior <51208117+JeffreyJuinior@users.noreply.github.com>
2025-03-15 17:33:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 App Name')
+st.title('Machine Learning App')
 
-st.write('Hello world!')
+st.info('This app will predict your obesity level!')
+
+df = pd.read_csv('https://raw.githubusercontent.com/JeffreyJuinior/dp-machinelearning/refs/heads/master/heart.csv')
+df
