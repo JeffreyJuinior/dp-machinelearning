d9e9a0b5520a83823f31cb0b7e2598a4bfc95b5f
Jeffrey Junior <51208117+JeffreyJuinior@users.noreply.github.com>
2025-03-15 21:57:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,22 +6,28 @@ model = joblib.load('trained_model.pkl')
 loaded_encoder = joblib.load('encoder.pkl')
 loeaded_scaler = joblib.load('scaler.pkl')
 
-st.title('Machine Learning App')
+def main():
+  st.title('Machine Learning App')
+  
+  st.info('This app will predict your obesity level!')
+  
+  # Raw Data
+  with st.expander('**Data**'):
+    st.write('This is a raw data')
+    df = pd.read_csv('https://raw.githubusercontent.com/JeffreyJuinior/dp-machinelearning/refs/heads/master/ObesityDataSet_raw_and_data_sinthetic.csv')
+    df
+    st.write('**X**')
+    X = df.drop('NObeyesdad',axis=1)
+    X
+    st.write('**y**')
+    y = df['NObeyesdad']
+    y
+  
+  # Visualization
+  with st.expander('**Data Visualization**'):
+    st.scatter_chart(data=df, x = 'Height', y = 'Weight', color='NObeyesdad')
+  
+  # Input Data
 
-st.info('This app will predict your obesity level!')
-
-with st.expander('**Data**'):
-  st.write('This is a raw data')
-  df = pd.read_csv('https://raw.githubusercontent.com/JeffreyJuinior/dp-machinelearning/refs/heads/master/ObesityDataSet_raw_and_data_sinthetic.csv')
-  df
-  st.write('**X**')
-  X = df.drop('NObeyesdad',axis=1)
-  X
-  st.write('**y**')
-  y = df['NObeyesdad']
-  y
-
-
-
-with st.expander('**Data Visualization**'):
-  st.scatter_chart(data=df, x = 'Height', y = 'Weight', color='NObeyesdad')
+def __input__ == "__main__":
+  main()
